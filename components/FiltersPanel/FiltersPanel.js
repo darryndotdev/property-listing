338a89d770719afaf2c23c388f29e767ba51bf74
Darryn <darryndotdev@gmail.com>
2025-11-17 14:09:30 +0000

    feat: creating controlled inputs for location and superhost and some basic styling

--- components/FiltersPanel/FiltersPanel.js
+++ components/FiltersPanel/FiltersPanel.js
@@ -1,7 +1,84 @@
+'use client';
+
+import { useState } from 'react';
 import styles from './FiltersPanel.module.css';
 
 function FiltersPanel() {
-    return <div className={styles.panel}>PANEL</div>;
+    const [location, setLocation] = useState('all');
+    const [isSuperHost, setIsSuperHost] = useState(true);
+
+    return (
+        <div className={styles.panel}>
+            <form className={styles.form}>
+                <fieldset className={styles.fieldset}>
+                    <label htmlFor='all' className={styles.label}>
+                        <input
+                            type='radio'
+                            id='all'
+                            name='input'
+                            value='all'
+                            className={styles.input}
+                            onChange={(e) => setLocation(e.target.value)}
+                            checked={location === 'all' ? true : ''}
+                        />
+                        <span className={styles.btn}>All stays</span>
+                    </label>
+                    <label htmlFor='norway' className={styles.label}>
+                        <input
+                            type='radio'
+                            id='norway'
+                            name='input'
+                            value='norway'
+                            className={styles.input}
+                            onChange={(e) => setLocation(e.target.value)}
+                        />
+                        <span className={styles.btn}>Norway</span>
+                    </label>
+                    <label htmlFor='finland' className={styles.label}>
+                        <input
+                            type='radio'
+                            id='finland'
+                            name='input'
+                            value='finland'
+                            className={styles.input}
+                            onChange={(e) => setLocation(e.target.value)}
+                        />
+                        <span className={styles.btn}>Finland</span>
+                    </label>
+                    <label htmlFor='sweden' className={styles.label}>
+                        <input
+                            type='radio'
+                            id='sweden'
+                            name='input'
+                            value='sweden'
+                            className={styles.input}
+                            onChange={(e) => setLocation(e.target.value)}
+                        />
+                        <span className={styles.btn}>Sweden</span>
+                    </label>
+                    <label htmlFor='switzerland' className={styles.label}>
+                        <input
+                            type='radio'
+                            id='switzerland'
+                            name='input'
+                            value='switzerland'
+                            className={styles.input}
+                            onChange={(e) => setLocation(e.target.value)}
+                        />
+                        <span className={styles.btn}>Switzerland</span>
+                    </label>
+                </fieldset>
+                <label>
+                    <input
+                        type='checkbox'
+                        onChange={() => setIsSuperHost(!isSuperHost)}
+                        checked={isSuperHost}
+                    />
+                    Superhost
+                </label>
+            </form>
+        </div>
+    );
 }
 
 export default FiltersPanel;
