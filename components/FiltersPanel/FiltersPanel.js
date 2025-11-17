be134895de6247995270190a6d6c755583c811b1
Darryn <darryndotdev@gmail.com>
2025-11-17 11:58:05 +0000

    styles: creating filter panel component and creating simple positioning styles for content overlap

---
+++ components/FiltersPanel/FiltersPanel.js
@@ -0,0 +1,7 @@
+import styles from './FiltersPanel.module.css';
+
+function FiltersPanel() {
+    return <div className={styles.panel}>PANEL</div>;
+}
+
+export default FiltersPanel;
--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -1,15 +1,19 @@
+import FiltersPanel from '../FiltersPanel/FiltersPanel';
 import styles from './Hero.module.css';
 
 function Hero() {
     return (
-        <header className={styles.header}>
-            <div>
-                <h1>Peace, nature, dream</h1>
-                <p className={styles.intro}>
-                    Find and book a great experience.
-                </p>
-            </div>
-        </header>
+        <div className={styles.relative}>
+            <header className={styles.header}>
+                <div>
+                    <h1>Peace, nature, dream</h1>
+                    <p className={styles.intro}>
+                        Find and book a great experience.
+                    </p>
+                </div>
+            </header>
+            <FiltersPanel />
+        </div>
     );
 }
 
